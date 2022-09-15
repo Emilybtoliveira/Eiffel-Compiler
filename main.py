580a9714a8ef048a44b8fa05735ef8bb3415fcee
Felipe Vasconcelos <felipevasconceloslpz@gmail.com>
2022-09-15 17:04:32 -0300

    Inicio Operators e Operators Sep
    Inicio do proceso de desenvolvimento de Operators e Operatos Separators, já separa, mas ainda tem um bug para os casos de "<<" ">>" da forma que está implementada.
    tambem falta a parte de delimitiadores e hierarchy

--- main.py
+++ main.py
@@ -144,45 +144,79 @@ def delimitersRecognizer(stack):
 
 
 def operatorsRecognizer(stack):
+    
     states = ['q0','qf','qintdivide','qintmenor','qintmaior','qintpontos']
     events = operators+[" ",'\n']
     meg = MEG(states,events,'q0','qf')
 
     meg.setTransitionRule('q0',["=", "+", "-","*","^", "."],'qf')
-    meg.setTransitionRule('q0',['/'],'qintdivide')
-    meg.setTransitionRule('qintdivide',['/','=',""],'qf')
+    meg.setTransitionRule('q0',['/'],'qf')
+    meg.setTransitionRule('qf',['/','='],'qf')
 
-    meg.setTransitionRule('q0',['<'],'qintmenor')
-    meg.setTransitionRule('qintmenor',['=',""],'qf')
+    meg.setTransitionRule('q0',['<'],'qf')
+    meg.setTransitionRule('qf',['=',""],'qf')
 
-    meg.setTransitionRule('q0',['>'],'qintmaior')
-    meg.setTransitionRule('qintmaior',['=',""],'qf')
+    meg.setTransitionRule('q0',['>'],'qf')
+    meg.setTransitionRule('qf',['=',""],'qf')
 
-    meg.setTransitionRule('q0',[':'],'qintpontos')
-    meg.setTransitionRule('qintpontos',['=',""],'qf')
+    meg.setTransitionRule('q0',[':'],'qf')
+    meg.setTransitionRule('qf',['=',""],'qf')
 
-    # tem que resolver a questao de como vai ser montado esses
-    # casos de dois digitos? tipo / e //, como vai ser no prox? se ele for vazio?
-    for index,char in enumerate(stack):
-        if char == "/":
-            if stack[index+1] in ['/','=']:
-                pass # lexema sao os dois juntos
-                meg.currentState = 'qf'
-            else:
-                pass # lexema eh ele sozinho
-                meg.currentState = 'qf'
 
-        if char == "<":
-            if stack[index+1] in ['=']:
-                pass # lexema sao os dois juntos
-                meg.currentState = 'qf'
-            else:
-                pass # lexema eh ele sozinho
-                meg.currentState = 'qf'
-                
+    for char in stack:
         meg.gotoNextState(char)
 
     return meg.recognizes()
+
+
+# Separators
+
+def hierarchy(stream):
+    print(stream)
+
+def op_separator(stream):
+    index = 0
+    word = ""
+    while index!=len(stream):
+        # Enquanto nao terminar a stream
+        # TODO: erro na parte de << e >>
+        char = stream[index]
+        if operatorsRecognizer(char):
+            # reconheceu, testar para o proximo
+            
+            # talvez tenha que fazer o esquema de ver se ta como operador e testar o proximo
+            
+            operatorLexeme = char
+            if index != len(stream)-1:
+                
+                nextCharStream = char+stream[index+1]
+            
+                if operatorsRecognizer(nextCharStream):
+                    # operator composto
+                    # aqui o autamto tem que saber reconhecer um // e nao um /-
+                    # falando que // eh valdio e /- nao é
+            
+                    operatorLexeme = nextCharStream
+                    index+=1
+                    
+            # mandar para a hierarquia o que ele ja achou
+            addNewToken(operatorLexeme,"opr")
+            operatorLexeme = ""
+            index+=1
+            hierarchy(word)
+            word = ""
+
+        else:
+            # nao reconheceu o atual, continua procurando
+            # ja vem sem os comentarios e espaço em branco
+            
+            word+=char
+            
+            index+=1
+
+    print(tokens_dict)
+
+
 
 # ----------------------------
 # TESTES UNITARIOS
@@ -191,7 +225,8 @@ def operatorsRecognizer(stack):
 def printTest(title, tests):
     print(f'Unit Testing: {title}')
     for resultKey, resultValue in tests.items():
-        print(f'    [{ "🟢" if (resultValue) else "🔴"}] {resultKey}')
+        # print(f'    [{ "🟢" if (resultValue) else "🔴"}] {resultKey}')
+        print(f'    [{ "R" if (resultValue) else "F"}] {resultKey}')
 
 
 def testIntegerRecognizer():
@@ -204,14 +239,32 @@ def testIntegerRecognizer():
         "rejectsLettersInTheEnd": not recognizesInteger("1234EF"),
         "rejectsSingleLetterInTheStart": not recognizesInteger("X23"),
         "rejectsSingleLetterInTheMiddle": not recognizesInteger("1Y3"),
-        "rejectsSingleLetterInTheEnd": not recognizesInteger("12Z"),
-        "acceptsComments":commentsRecognizer("-- 123asdad"),
-        "acceptsDelimiters":delimitersRecognizer(">>"),
-        "acceptsOperators":operatorsRecognizer("/")
+        "rejectsSingleLetterInTheEnd": not recognizesInteger("12Z")
     }
-    tests.items()
     printTest("IntegerRecognizer", tests)
 
+def testCommentsRecognizer():
+    tests = {
+        "acceptsComments":commentsRecognizer("-- 123asdad")
+    }
+    printTest("CommentsRecognizer",tests)
+
+def testDelimitersRecognizer():
+    tests = {
+        "acceptsDelimiters":delimitersRecognizer(">>")
+    }
+    printTest("CommentsRecognizer",tests)
+    
+
+def testOperatorsRecognizer():
+    tests = {
+        "acceptsOperatorsTwoRight":operatorsRecognizer("//"),
+        "acceptsOperatorsTwoWrong":operatorsRecognizer("/-"),
+        "acceptsOperatorsTwoWrong2":operatorsRecognizer("/a"),
+        "acceptsOperators":operatorsRecognizer(">>")
+    }
+    printTest("CommentsRecognizer",tests)
+    
 
 
 
@@ -223,7 +276,7 @@ def testIntegerRecognizer():
 
 def main():
     source_code = loadSourceCode()
-
+    
     # ordem de chamada dos reconhecedores
     # TODO: commentsRecognizer()
     # TODO: delimitersRecognizer()
@@ -231,8 +284,10 @@ def main():
     # TODO: idRecognizer()  # aqui verifica-se se é uma palavra reservada
     # DONE: integerRecognizer()
 
+    op_separator("age:= integer age = -10 << tteste >>")
 
 
 if __name__ == '__main__':
     testIntegerRecognizer()
+    testOperatorsRecognizer()
     main()
