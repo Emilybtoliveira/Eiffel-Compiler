beff59107c6a7d37ee9eb59ab498be932dcdbdc7
TaigoI <timp@ic.ufal.br>
2022-09-18 19:19:42 -0300

    Descrição q eu n tenho energia pra escrever...

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import string
 
 delimiters = ["(", ")", "{", "}", "[", "]", ";", ",", "<<", ">>", "do", "end"]
 tokens_dict = {}
-reserved_words = ["feature", "none", "assign", "current", "create","class",
+reserved_words = ["feature", "none", "assign", "current", "create", "class",
                   "loop", "from", "until", "all", "some", "integer", "array"]
 operators = ["=", "/=", "<", ">", "<=", ">=", "+", "-", "not",
              "*", "/", "//", "^", "and", "or", ":=", "assign", ":", "."]
@@ -76,7 +76,6 @@ def addNewToken(lexeme, lexeme_class):
         return
 
     tokens_dict[lexeme] = lexeme_class
-    # print(tokens_dict)
 
 
 def throwError(error_position, error_message):
@@ -103,49 +102,54 @@ def recognizesInteger(stack):
 
     return meg.recognizes()
 
+
 def recognizesReserved(stack):
     return stack in reserved_words
 
-def classifyToken(stack):
+
+def classifiesToken(stack):
     if(recognizesInteger(stack)):
-        return "int"
+        addNewToken(stack, "int")
     elif(recognizesReserved(stack)):
-        return "reserved"
-    return "id"
+        addNewToken(stack, "reserved")
+    else:
+        addNewToken(stack, "id")
+
 
 def commentsRecognizer(stack):
-    
-    # Ver a questao dos caracteres 
 
-    states = ['q0','qint','qf']
+    # Ver a questao dos caracteres
+
+    states = ['q0', 'qint', 'qf']
     digit_events = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     alphabet_events = list(string.ascii_lowercase)
-    events = ['\n',' ']+digit_events+alphabet_events # aqui tem que ser o -, osd igitos e o alfabeto
+    # aqui tem que ser o -, osd igitos e o alfabeto
+    events = ['\n', ' ']+digit_events+alphabet_events
     meg = MEG(states, events+['-'], "q0", "qf")
-    meg.setTransitionRule('q0',['-'],'qint')
-    meg.setTransitionRule('qint','-','qf')
-    meg.setTransitionRule('qf',events,'qf')
-    
+    meg.setTransitionRule('q0', ['-'], 'qint')
+    meg.setTransitionRule('qint', '-', 'qf')
+    meg.setTransitionRule('qf', events, 'qf')
+
     for char in stack:
         meg.gotoNextState(char)
-    
+
     return meg.recognizes()
 
 
 def delimitersRecognizer(stack):
-    states = ['q0','qf','qintmenor','qintmaior']
+    states = ['q0', 'qf', 'qintmenor', 'qintmaior']
     delimitersOneDigit = delimiters[:8]
-    events = delimitersOneDigit+[' ','\n','>',"<"]
-    meg = MEG(states,events,'q0','qf')
+    events = delimitersOneDigit+[' ', '\n', '>', "<"]
+    meg = MEG(states, events, 'q0', 'qf')
 
-    # >> e << 
-    meg.setTransitionRule('q0','>','qintmaior')
-    meg.setTransitionRule('qintmaior','>','qf')
-    meg.setTransitionRule('q0','<','qintmenor')
-    meg.setTransitionRule('qintmenor','<','qf')
-    
+    # >> e <<
+    meg.setTransitionRule('q0', '>', 'qintmaior')
+    meg.setTransitionRule('qintmaior', '>', 'qf')
+    meg.setTransitionRule('q0', '<', 'qintmenor')
+    meg.setTransitionRule('qintmenor', '<', 'qf')
+
     # delimitadores com um unico digito, logo vai pro final
-    meg.setTransitionRule('q0',delimitersOneDigit,'qf')
+    meg.setTransitionRule('q0', delimitersOneDigit, 'qf')
 
     for char in stack:
         meg.gotoNextState(char)
@@ -154,24 +158,23 @@ def delimitersRecognizer(stack):
 
 
 def operatorsRecognizer(stack):
-    
-    states = ['q0','qf','qintdivide','qintmenor','qintmaior','qintpontos']
-    events = operators+[" ",'\n']
-    meg = MEG(states,events,'q0','qf')
 
-    meg.setTransitionRule('q0',["=", "+", "-","*","^", "."],'qf')
-    meg.setTransitionRule('q0',['/'],'qf')
-    meg.setTransitionRule('qf',['/','='],'qf')
+    states = ['q0', 'qf', 'qintdivide', 'qintmenor', 'qintmaior', 'qintpontos']
+    events = operators+[" ", '\n']
+    meg = MEG(states, events, 'q0', 'qf')
 
-    meg.setTransitionRule('q0',['<'],'qf')
-    meg.setTransitionRule('qf',['=',""],'qf')
+    meg.setTransitionRule('q0', ["=", "+", "-", "*", "^", "."], 'qf')
+    meg.setTransitionRule('q0', ['/'], 'qf')
+    meg.setTransitionRule('qf', ['/', '='], 'qf')
 
-    meg.setTransitionRule('q0',['>'],'qf')
-    meg.setTransitionRule('qf',['=',""],'qf')
+    meg.setTransitionRule('q0', ['<'], 'qf')
+    meg.setTransitionRule('qf', ['=', ""], 'qf')
 
-    meg.setTransitionRule('q0',[':'],'qf')
-    meg.setTransitionRule('qf',['=',""],'qf')
+    meg.setTransitionRule('q0', ['>'], 'qf')
+    meg.setTransitionRule('qf', ['=', ""], 'qf')
 
+    meg.setTransitionRule('q0', [':'], 'qf')
+    meg.setTransitionRule('qf', ['=', ""], 'qf')
 
     for char in stack:
         meg.gotoNextState(char)
@@ -181,123 +184,125 @@ def operatorsRecognizer(stack):
 
 # Separators
 
-def hierarchy(stream,level):
+def hierarchy(stream, level):
     if level == 0:
         comments_separator(stream)
     elif level == 1:
         op_separator(stream)
     elif level == 2:
         delimiters_separator(stream)
-    
+    elif level == 3:
+        classifiesToken(stream)
 
 
 def comments_separator(stream):
 
     # se tiver um comentario, ele vai ignorar de -- ate o fim da linha
     word = ""
-    for index,char in enumerate(stream):
-        if index!=(len(stream)-1):
+    for index, char in enumerate(stream):
+        if index != (len(stream)-1):
             combination = char+stream[index+1]
             if commentsRecognizer(combination):
                 # reconheceu, lgo descarta o resto
                 word = stream[0:index]
                 break
+            else:
+                word += char
+        else:
+            word += char
+    hierarchy(word, 1)
 
-    hierarchy(word,1)
-    
 
 def op_separator(stream):
     # operator ta comendo o << e >> quando manda pra o delimiter
-    
+
     index = 0
     word = ""
-    while index!=len(stream):
+    while index != len(stream):
 
         char = stream[index]
         if operatorsRecognizer(char):
             # reconheceu, testar para o proximo
             operatorLexeme = char
             if index != len(stream)-1:
-                
+
                 nextCharStream = char+stream[index+1]
-            
+
                 if operatorsRecognizer(nextCharStream):
                     # operator composto
                     operatorLexeme = nextCharStream
-                    index+=1
+                    index += 1
 
                 elif delimitersRecognizer(nextCharStream):
                     # eh um delimitador, nao aceitar
-                        operatorLexeme = ""
-                        word+=nextCharStream
-                        index+=1
-                        
+                    operatorLexeme = ""
+                    word += nextCharStream
+                    index += 1
 
             # mandar para a hierarquia o que ele ja achou
             if operatorLexeme != "":
-                addNewToken(operatorLexeme,"opr")
+                addNewToken(operatorLexeme, "opr")
                 operatorLexeme = ""
-            
-            hierarchy(word,2)
-            index+=1
+
+            hierarchy(word, 2)
+            index += 1
             word = ""
 
         else:
             # nao reconheceu o atual, continua procurando
             # ja vem sem os comentarios e espaço em branco
-            word+=char
-            index+=1
+            word += char
+            index += 1
+    hierarchy(word, 2)
 
-    
 
 def delimiters_separator(stream):
-    
+
     index = 0
     word = ""
-    while index!=len(stream):
-        
+    while index != len(stream):
+
         char = stream[index]
-        
-        if char in ["<",">"]:
-            
+
+        if char in ["<", ">"]:
+
             # reconheceu, testar para o proximo
             delimiterLexeme = char
-            
+
             if index != len(stream)-1:
-                
+
                 nextCharStream = char+stream[index+1]
-            
+
                 if delimitersRecognizer(nextCharStream):
                     # delimiter composto
                     delimiterLexeme = nextCharStream
-                    index+=1
-                    addNewToken(delimiterLexeme,"del")
-            
+                    index += 1
+                    addNewToken(delimiterLexeme, "del")
+
                     # se nao for, é simples, logo, nao é delimitador
-            
+
             delimiterLexeme = ""
-            index+=1
-            hierarchy(word,3)
+            index += 1
+            hierarchy(word, 3)
             word = ""
         elif char in delimiters:
-            index+=1
-            addNewToken(char,"del")
+            index += 1
+            addNewToken(char, "del")
         else:
             # nao reconheceu o atual, continua procurando
             # ja vem sem os comentarios e espaço em branco
-            word+=char
-            index+=1
-
+            word += char
+            index += 1
 
 # ----------------------------
 # TESTES UNITARIOS
 # ----------------------------
 
+
 def printTest(title, tests):
     print(f'Unit Testing: {title}')
     for resultKey, resultValue in tests.items():
         print(f'    [{ "🟢" if (resultValue) else "🔴"}] {resultKey}')
-        
 
 
 def testIntegerRecognizer():
@@ -314,6 +319,7 @@ def testIntegerRecognizer():
     }
     printTest("IntegerRecognizer", tests)
 
+
 def testReservedRecognizer():
     tests = {
         "acceptsClass": recognizesReserved("class"),
@@ -322,30 +328,32 @@ def testReservedRecognizer():
     }
     printTest("ReservedRecognizer", tests)
 
+
 def testCommentsRecognizer():
     tests = {
-        "acceptsComments":commentsRecognizer("-- 123asdad")
+        "acceptsComments": commentsRecognizer("-- 123asdad")
     }
-    printTest("CommentsRecognizer",tests)
+    printTest("CommentsRecognizer", tests)
+
 
 def testDelimitersRecognizer():
     tests = {
-        "acceptsDelimitersSingle":delimitersRecognizer(">"),
-        "acceptsDelimitersDouble":delimitersRecognizer(">>"),
-        "acceptsDelimitersWrong":delimitersRecognizer("\s")
+        "acceptsDelimitersSingle": delimitersRecognizer(">"),
+        "acceptsDelimitersDouble": delimitersRecognizer(">>"),
+        "acceptsDelimitersWrong": delimitersRecognizer("\s")
     }
-    printTest("DelimitersRecognizer",tests)
-    
+    printTest("DelimitersRecognizer", tests)
+
 
 def testOperatorsRecognizer():
     tests = {
-        "acceptsOperatorsTwoRight":operatorsRecognizer("//"),
-        "acceptsOperatorsTwoWrong":operatorsRecognizer("/-"),
-        "acceptsOperatorsTwoWrong2":operatorsRecognizer("/a"),
-        "acceptsOperators":operatorsRecognizer(">>")
+        "acceptsOperatorsTwoRight": operatorsRecognizer("//"),
+        "acceptsOperatorsTwoWrong": operatorsRecognizer("/-"),
+        "acceptsOperatorsTwoWrong2": operatorsRecognizer("/a"),
+        "acceptsOperators": operatorsRecognizer(">>")
     }
-    printTest("CommentsRecognizer",tests)
-    
+    printTest("CommentsRecognizer", tests)
+
 
 # ----------------------------
 # LOOP PRINCIPAL
@@ -354,17 +362,10 @@ def testOperatorsRecognizer():
 
 def main():
     source_code = loadSourceCode()
-    
-    # ordem de chamada dos reconhecedores
-    # TODO: idRecognizer()  # aqui verifica-se se é uma palavra reservada
-    # DONE: delimitersRecognizer()
-    # DONE: commentsRecognizer()
-    # DONE: operatorsRecognizer()
-    # DONE: integerRecognizer()
-    
-    frase = "age:integer a vida>10 e tem tambem () e {} e <= e ! // asd << teste >> -- tchau "
-    hierarchy(frase,0)
-    print(frase)
+    lines = source_code.split("\n")
+
+    for line in lines:
+        hierarchy(line, 0)
     print(tokens_dict)
 
 
