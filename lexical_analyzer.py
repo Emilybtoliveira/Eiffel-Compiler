3e491b9b8bf9e0d2631369e49dc78c61d0ba9fa8
TaigoI <taigo.pedrosa@gmail.com>
2022-12-18 17:34:56 -0300

    autopep8 e parseLine para pegar os tokens da linha

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -11,8 +11,10 @@ source_code = ""
 tokens_list = []
 
 delimiters = ["(", ")", "[", "]", ",", "<<", ">>"]
-reserved_words = ["feature", "create", "class", "integer","loop", "from", "until", "array", "do", "end", "not", "and", "or", "if", "then", "else"]
-operators = ["=", "/=", "<", ">", "<=", ">=", "+", "-","*", "/", "//", "^", ":=", ":", "."]
+reserved_words = ["feature", "create", "class", "integer", "loop", "from",
+                  "until", "array", "do", "end", "not", "and", "or", "if", "then", "else"]
+operators = ["=", "/=", "<", ">", "<=", ">=",
+             "+", "-", "*", "/", "//", "^", ":=", ":", "."]
 
 
 @dataclass()
@@ -21,18 +23,21 @@ class Token:
     lexeme_class: string
 
 # ============ RECONHECEDORES ============
+
+
 def checksNewLineAndSpace():
     global current_position, line_count
-    
-    while(source_code[current_position] == "\n" or source_code[current_position] == " "):
+
+    while (source_code[current_position] == "\n" or source_code[current_position] == " "):
         if source_code[current_position] == "\n":
             line_count += 1
             #print(f"linha {line_count}")
-        
+
         current_position += 1
 
         if current_position >= len(source_code):
             break
+
 
 def commentRecognizer():
     global current_position
@@ -46,100 +51,106 @@ def commentRecognizer():
             lexeme += "--"
             current_position += 2
 
-            while(source_code[current_position] != "\n"):
+            while (source_code[current_position] != "\n"):
                 lexeme += source_code[current_position]
                 current_position += 1
 
                 if current_position >= len(source_code):
                     break
-            
+
             new_token = Token(lexeme, "comment")
-            #print(f"{new_token}")
+            # print(f"{new_token}")
             tokens_list.append(new_token)
 
             return True
-    
+
     return False
+
 
 def delimiterRecognizer():
     global current_position
     #print("Iniciando reconhecedor de delimitadores...")
-    lexeme = ""         
+    lexeme = ""
 
     checksNewLineAndSpace()
-   
+
     if source_code[current_position] in delimiters:
         lexeme += source_code[current_position]
         current_position += 1
-    elif current_position+1 < len(source_code):    
+    elif current_position+1 < len(source_code):
         if (source_code[current_position]+source_code[current_position+1]) in delimiters:
-            lexeme += source_code[current_position]+source_code[current_position+1]
+            lexeme += source_code[current_position] + \
+                source_code[current_position+1]
             current_position += 2
         else:
             return False
     else:
         return False
-    
+
     new_token = Token(lexeme, "del")
-    #print(f"{new_token}")
+    # print(f"{new_token}")
     tokens_list.append(new_token)
     return True
+
 
 def operatorRecognizer():
     global current_position
     #print("Iniciando reconhecedor de operadores...")
-    lexeme = ""         
+    lexeme = ""
 
     checksNewLineAndSpace()
 
     if current_position+1 < len(source_code):
         if (source_code[current_position]+source_code[current_position+1]) in operators:
-            lexeme += source_code[current_position]+source_code[current_position+1]
-            
+            lexeme += source_code[current_position] + \
+                source_code[current_position+1]
+
             current_position += 2
             new_token = Token(lexeme, "opr")
-            #print(f"{new_token}")
+            # print(f"{new_token}")
             tokens_list.append(new_token)
-            
+
             return True
-        
+
     if source_code[current_position] in operators:
-            lexeme += source_code[current_position]
+        lexeme += source_code[current_position]
 
-            current_position += 1
-            new_token = Token(lexeme, "opr")
-            #print(f"{new_token}")
-            tokens_list.append(new_token)
+        current_position += 1
+        new_token = Token(lexeme, "opr")
+        # print(f"{new_token}")
+        tokens_list.append(new_token)
 
-            return True
+        return True
     else:
         return False
-    
+
+
 def numberRecognizer():
     global current_position
     #print("Iniciando reconhecedor de números...")
-    lexeme = ""         
+    lexeme = ""
 
     checksNewLineAndSpace()
 
-    if ord(source_code[current_position]) >= 48 and  ord(source_code[current_position]) <= 57:
-        while ord(source_code[current_position])  >= 48 and ord(source_code[current_position]) <= 57:
+    if ord(source_code[current_position]) >= 48 and ord(source_code[current_position]) <= 57:
+        while ord(source_code[current_position]) >= 48 and ord(source_code[current_position]) <= 57:
             lexeme += source_code[current_position]
             current_position += 1
 
             if current_position >= len(source_code):
                 break
-    
+
         new_token = Token(lexeme, "int")
-        #print(f"{new_token}")
+        # print(f"{new_token}")
         tokens_list.append(new_token)
-        return True    
+        return True
     return False
+
 
 def identifierRecognizer():
     global current_position
     #print("Iniciando reconhecedor de identificadores...")
-    lexeme = ""         
+    lexeme = ""
 
     checksNewLineAndSpace()
 
@@ -150,71 +161,101 @@ def identifierRecognizer():
 
             if current_position >= len(source_code):
                 break
-    
+
         if lexeme.lower() in reserved_words:
             new_token = Token(lexeme, "reserved")
         else:
-            new_token = Token(lexeme, "id")            
-        #print(f"{new_token}")
+            new_token = Token(lexeme, "id")
+        # print(f"{new_token}")
         tokens_list.append(new_token)
-        return True    
-    
+        return True
+
     return False
 
 # ============ FUNÇÕES GERAIS ============
+
+
 def throwError():
-    global current_position,line_count
-    
+    global current_position, line_count
+
     print(f'An error has been found in line {line_count+1}.')
-    print(f'See description: Word {source_code[current_position]} not recognized.\n')
-    current_position+=1
+    print(
+        f'See description: Word {source_code[current_position]} not recognized.\n')
+    current_position += 1
 
 
 def loadSourceCode():
     with open("code.txt", "r") as source:
         return source.read()
 
+
 def iterativeScanner(source_code):
 
     try:
         for i in range(len(source_code)):
-            
+
             if not commentRecognizer():
                 #print("Não é comentário.")
-                
+
                 if not delimiterRecognizer():
                     #print("Não é delimitador.")
-                    
+
                     if not operatorRecognizer():
                         #print("Não é operador.")
 
                         if not numberRecognizer():
                             #print("Não é número.")
 
-                            if not identifierRecognizer():        
+                            if not identifierRecognizer():
                                 #print("Não é identificador.")
-                                
+
                                 throwError()
     except IndexError:
-        return()
+        return ()
+
 
 def printTokens():
+    output = ""
     for token in tokens_list:
+        output = output + str(token) + "\n"
         print(token)
 
-def main(debug=False):
+    with open("tokens.txt", "w") as source:
+        return source.write(output)
+
+
+def resetGlobals():
+    global source_code, current_position, line_count, tokens_list
+    source_code = ""
+    current_position = 0
+    line_count = 0
+    tokens_list = []
+
+
+def parseLine(line):
+    resetGlobals()
+    global source_code
+
+    source_code = line
+    iterativeScanner(source_code)
+    return tokens_list
+
+
+def main(debug=False, code: str = None):
+    resetGlobals()
     global source_code
 
     try:
-        source_code = loadSourceCode()
+        source_code = code if code != None else loadSourceCode()
     except:
         print("\nErro: Não foi possível ler o arquivo.")
-    else:        
+    else:
         iterativeScanner(source_code)
         if debug:
             printTokens()
-        
+
         return tokens_list
+
 
 if __name__ == '__main__':
     main(True)
